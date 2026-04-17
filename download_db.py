bff232dac9b9ef8aac1dc027f84982102a4b4099
HARDIK CHAUDHARY <hardik10chaudhary@gmail.com>
2026-04-17 21:21:31 +0530

    Update download_db.py

--- download_db.py
+++ download_db.py
@@ -1,9 +1,23 @@
 import gdown
+import os
 
-folder_id = "128VOydnhhONOJY_S-yh-i2xWB9VDus18"
+os.makedirs("data", exist_ok=True)
 
-gdown.download_folder(
-    id=folder_id,
-    output="data",
-    quiet=False
-)
+file_path = "data/inventory.db"
+
+# Check if file already exists
+if os.path.exists(file_path):
+    print("✅ Database already exists. Skipping download.")
+else:
+    print("⬇️ Downloading database...")
+
+    file_id = "1-Recy6FYA8j8JlOHQ4Se1FUWdtvu8hrj"
+    url = f"https://drive.google.com/uc?id={file_id}"
+
+    try:
+        gdown.download(url, file_path, quiet=False)
+        print("✅ Download completed successfully!")
+    except Exception as e:
+        print("❌ ERROR: Unable to download file.")
+        print("👉 Make sure file is public (Anyone with link).")
+        print(e)
